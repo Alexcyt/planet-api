1311d522b77f86ef9304d1dc5f19eaf171044a7e
Alexcyt <ictcyt@163.com>
2018-01-03 21:05:30 +0800

    project init

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const Koa = require('koa');
-const app = new Koa();
 const config = require('config');
+const Router = require('koa-router');
+const session = require('koa-session');
+const log = require('./lib/log');
+const koaLogger = require('koa-logger');
 
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
+const app = new Koa();
+const router = new Router();
+
+app.keys = [config.sessionSecretKey];
+
+app.use(koaLogger());
+app.use(session(app));
+
+router.get('/', async (ctx) => {
+  let n = ctx.session.views || 0;
+  ++n;
+  ctx.session.views = n;
+  ctx.body = `${n} views`;
 });
 
+router.get('/logout', async ctx => {
+  ctx.session = null;
+  ctx.body = 'Goodbye';
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
 app.listen(config.listenPort);
+log.info(`Server listen at port ${config.listenPort}`);
